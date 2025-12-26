d8b5df4cbe09a17e8424d3ede45350686bc543c8
Steve Brand <thatfanficguy@protonmail.com>
2025-12-26 15:35:59 +0100

    Add `<FitText />`

---
+++ components/FitText.tsx
@@ -0,0 +1,38 @@
+import { cn } from "@/lib/utils";
+import type { CSSProperties, FunctionComponent, HTMLAttributes } from "react";
+
+type FitTextProps = HTMLAttributes<HTMLSpanElement> & {
+	/**
+	 * Maximum width of the text, as a CSS length string. See examples below.
+	 * The contents of the component will not grow beyond the stated width if `max` is provided.
+	 * If not provided, the text will scale to the limits of its container.
+	 *
+	 * @example "12rem"
+	 * @example "800px"
+	 * @example "var(--max-text-size)"
+	 */
+	max?: string;
+};
+
+export const FitText: FunctionComponent<FitTextProps> = ({
+	children,
+	max,
+	className,
+	style,
+	...props
+}) => {
+	return (
+		<span
+			className={cn("fit-text flex @container", className)}
+			style={{ "--fit-text-max": max, ...(style as CSSProperties) }}
+			{...props}
+		>
+			<span className="fit-text-container grow @container">
+				<span className="fit-text-display block">{children}</span>
+			</span>
+			<span aria-hidden="true" className="invisible">
+				{children}
+			</span>
+		</span>
+	);
+};
